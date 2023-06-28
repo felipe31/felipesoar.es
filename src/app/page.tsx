155e80d4eab7420270d54a15a232c11666708f6c
Felipe Soares <felipe31soares@gmail.com>
2023-06-28 14:29:26 -0300

    Parse README

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,26 +8,29 @@ import SideDrawer from "./components/SideDrawer";
 import { CssBaseline } from "@mui/material";
 import { Box } from "@mui/system";
 import CardList from "./components/CardList";
-import { getReadMe } from "@/utilities";
+import { Project, getReadMe, parseReadMe } from "@/utilities";
 import { useState } from "react";
 
 export default async function Home() {
-  const list = [
-    {
-      img: "/lizard.jpg",
-      title: "Project 1",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    },
-    {
-      img: "/lizard.jpg",
-      title: "Project 2",
-      description:
-        "Lorem atur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    },
+  const list: Project[] = [
+    //   {
+    //     img: "/lizard.jpg",
+    //     title: "Project 1",
+    //     description:
+    //       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    //   },
+    //   {
+    //     img: "/lizard.jpg",
+    //     title: "Project 2",
+    //     description:
+    //       "Lorem atur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    //   },
   ];
 
-  // await getReadMe("");
+  list.push(parseReadMe(await getReadMe("felipe31", "githubbrowser")));
+  list.push(parseReadMe(await getReadMe("felipe31", "quincunx-game")));
+  list.push(parseReadMe(await getReadMe("felipe31", "SistemaVotos")));
+  list.push(parseReadMe(await getReadMe("felipe31", "AlfaBooks")));
 
   return (
     <main className={styles.main}>
--- src/utilities.ts
+++ src/utilities.ts
@@ -4,18 +4,67 @@ export interface Project {
   description: string;
 }
 
-export async function getReadMe(repoURL: string) {
-  const res = await fetch(`https://raw.githubusercontent.com/felipe31/githubbrowser/main/README.md`);
+export async function getReadMe(user: string, repo: string) {
+  let url = `https://raw.githubusercontent.com/${user}/${repo}/`;
+  let readMe = "";
+  const possibleBranches = ["main", "master"];
+  for (const branch of possibleBranches) {
+    try {
+      const res = await fetch(`${url}${branch}/README.md`);
 
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error("Failed to fetch data");
+      if (!res.ok) {
+        throw new Error("Failed to fetch data");
+      }
+      readMe = await res.text();
+      url = `${url}${branch}/`;
+      break;
+    } catch (e) {}
   }
 
-  console.log(res.body);
-  return res.body;
+  // console.log(text);
+  return { readMe, url };
 }
 
-export function parseReadMe(readMe: string): Project {
-  return { img: "", title: "", description: "" };
+export function parseReadMe(props: { readMe: string; url: string }): Project {
+  const _toReturn = { img: "/lizard.jpg", title: "Title here", description: "Description here" };
+
+  // Parse title
+  console.log("Parse title");
+  const titleRegex = new RegExp(/(?:^##?\s)([\s\S]*?)(?:(\s!\[.*?)?(\n))/);
+  if (titleRegex.test(props.readMe)) {
+    const res = titleRegex.exec(props.readMe);
+    if (res) {
+      _toReturn.title = res[1];
+    }
+  }
+  console.log(titleRegex.test(props.readMe));
+
+  // Parse description
+  console.log("Parse description");
+  const descriptionRegex = new RegExp(/(?:^(?:\n|\r))*(^[^#][\s\S]*?)(?:(\n|\r))/m);
+  if (descriptionRegex.test(props.readMe)) {
+    const res = descriptionRegex.exec(props.readMe);
+    if (res) {
+      _toReturn.description = res[1];
+    }
+  }
+  console.log(descriptionRegex.test(props.readMe));
+
+  // Parse image
+  console.log("Parse image");
+  const imageRegex = new RegExp(/(?:^\![\s\S]+?\()(.*?)(?:\))/m);
+  if (imageRegex.test(props.readMe)) {
+    const res = imageRegex.exec(props.readMe);
+    if (res) {
+      _toReturn.img = res[1];
+    }
+    console.log(res);
+    if (_toReturn.img.startsWith("./")) {
+      _toReturn.img = `${props.url}${_toReturn.img}`;
+    }
+  }
+  console.log(imageRegex.test(props.readMe));
+
+  console.log(_toReturn);
+  return _toReturn;
 }
