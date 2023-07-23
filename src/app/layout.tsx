fbddb94db5bcf281533bc5eeeb1822ae954ee827
Felipe Soares <felipe31soares@gmail.com>
2023-07-23 16:03:45 -0300

    Improve CSS

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </head>
